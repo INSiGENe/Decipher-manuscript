44a9ca3d590b6c1085ecd4d79dfd1353e0ca1ea0
Edgar Basto Piamonte <ea.basto@gmail.com>
2025-08-06 01:38:45 +1000

    added some lines ot our pipeline commands to run figures 2 to 5

--- pipeline_commands/4_benchmarking and visualization.sh
+++ pipeline_commands/4_benchmarking and visualization.sh
@@ -2,6 +2,10 @@ docker pull ebasto/decipher-manuscript-decipherc2c@sha256:001305292bc84a0dfee079
 
 sudo docker run -it -v "$(pwd):/workspace" -w /workspace ebasto/decipher-manuscript-decipherc2c@sha256:001305292bc84a0dfee0799a6bbb0f687c3d8bb2e58b2092751bbb8982a38d46
 source("scripts/benchmarking_and_figure_scripts/1_load_all_results.r")
+source("scripts/benchmarking_and_figure_scripts/figure_2.R")
+source("scripts/benchmarking_and_figure_scripts/figure_3.R")
+source("scripts/benchmarking_and_figure_scripts/figure_4.R")
+source("scripts/benchmarking_and_figure_scripts/figure_5.R")
 
 
 sudo docker run -it -v "$(pwd):/workspace" -w /workspace ebasto/decipher-manuscript-decipherc2c@sha256:001305292bc84a0dfee0799a6bbb0f687c3d8bb2e58b2092751bbb8982a38d46
