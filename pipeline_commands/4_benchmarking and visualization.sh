e9a8cf4d7d543350fc08232a7e42f8285e39f7d2
Edgar Basto Piamonte <ea.basto@gmail.com>
2025-08-12 10:55:22 +1000

    minor changes

--- pipeline_commands/4_benchmarking and visualization.sh
+++ pipeline_commands/4_benchmarking and visualization.sh
@@ -1,9 +1,15 @@
 docker pull ebasto/decipher-manuscript-decipherc2c@sha256:001305292bc84a0dfee0799a6bbb0f687c3d8bb2e58b2092751bbb8982a38d46
 
+#the following R scripts are dependent, i.e. figure 2 depends on load_all_results
 sudo docker run -it -v "$(pwd):/workspace" -w /workspace ebasto/decipher-manuscript-decipherc2c@sha256:001305292bc84a0dfee0799a6bbb0f687c3d8bb2e58b2092751bbb8982a38d46
 source("scripts/benchmarking_and_figure_scripts/1_load_all_results.r")
 source("scripts/benchmarking_and_figure_scripts/figure_2.R")
+
+
 source("scripts/benchmarking_and_figure_scripts/figure_3.R")
+
+#the following R scripts are independent, i.e. figure 5 does not depend on figure 4
+sudo docker run -it -v "$(pwd):/workspace" -w /workspace ebasto/decipher-manuscript-decipherc2c@sha256:001305292bc84a0dfee0799a6bbb0f687c3d8bb2e58b2092751bbb8982a38d46
 source("scripts/benchmarking_and_figure_scripts/figure_4.R")
 source("scripts/benchmarking_and_figure_scripts/figure_5.R")
 
