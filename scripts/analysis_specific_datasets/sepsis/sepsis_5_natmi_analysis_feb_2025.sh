027829def2bdfa429ebd9969bbcbfe2f4b319261
Edgar Andres Basto Piamonte <ea.basto@gmail.com>
2025-08-30 17:08:53 +1000

    removing manuscript scripts from v2, which is Decipher specific"

--- scripts/analysis_specific_datasets/sepsis/sepsis_5_natmi_analysis_feb_2025.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cd NATMI
-DATASET=sepsis
-python3 ExtractEdges.py --species human --emFile ../results/$DATASET/natmi/data/case/em.txt --annFile ../results/$DATASET/natmi/data/case/metadata.txt --interDB lrc2p --coreNum 4 --out ../results/$DATASET/natmi/data/case/
-python3 ExtractEdges.py --species human --emFile ../results/$DATASET/natmi/data/control/em.txt --annFile ../results/$DATASET/natmi/data/control/metadata.txt --interDB lrc2p --coreNum 4 --out ../results/$DATASET/natmi/data/control/
-python3 DiffEdges.py --refFolder ../results/$DATASET/natmi/data/control/ --targetFolder ../results/$DATASET/natmi/data/case/ --interDB lrc2p --out ../results/$DATASET/natmi/data/diff/
